73effe83838e904587de8edb6f12d39fd566bfa2
Gitselin <mselin@protonmail.com>
2018-11-22 11:57:01 +0100

    Set up of basic file structure

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,4 +1,7 @@
-public class Main {
+package Controller;
+
+public class Controller {
     public static void main(String[] args) {
+
     }
 }
---
+++ src/Model/Screen.java
@@ -0,0 +1,11 @@
+package Model;
+
+public class Screen {
+    int ID;
+    String name;
+    String model;
+    boolean stateOn;
+    int stateUptime;
+    int stateTemp;
+
+}
